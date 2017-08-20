8d307896dc7ede1145eef2776ac18a502b2db256
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-08-20 23:38:07 +0530

    Initial commit

---
+++ ideone_UcW9oC.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, sum;
+  cout << "Enters two numbers to find their sum : ";
+  cin >> a >> b;
+  sum = a + b;
+  cout << "The sum is " << sum;
+}
