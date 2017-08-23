8cc57f9a6df66ea5c344aef77698f68dd48b51cc
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-08-23 15:10:51 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	  int l, b, perimeter;
+  cout << "Enters length and breadth : ";
+  cin >> l >> b;
+  
+  perimeter = 2 * ( l + b );
+  cout << "The perimeter is " << perimeter;
+ 	return 0;
+}
