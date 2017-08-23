185c03ebadc873385d02dc751b7e8d6ff85542dc
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-08-23 15:54:39 +0530

    Update and rename 6.cpp to Problem 6.cpp

--- Problem 6.cpp
+++ Problem 6.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() 
 {
 	  float r, diameter, circumference, area;
-  cout << "Enter radius of the circle : ";
+  cout << "Enter radius of the circle : "<<endl;
   cin >> r;
   
   diameter = 2 * r;
-  cout << "The diameter is " << diameter;
+  cout << "The diameter is " << diameter<<endl;
   
   circumference = 2 * 3.14 * r;
-  cout << "The diameter is " << circumference;
+  cout << "The diameter is " << circumference<<endl;
   
   area = 3.14 * r * r ;
   cout << " The area is " << area;
