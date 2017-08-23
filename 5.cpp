7e78a8242012ff470db4ec51540ce497e04595a9
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-08-23 15:12:31 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	  int l, b, area;
+  cout << "Enters length and breadth : ";
+  cin >> l >> b;
+  
+  area = l * b;
+  cout << "The area is " << area;
+ 	return 0;
+}
