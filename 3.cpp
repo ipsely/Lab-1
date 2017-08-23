59d123865f8e9915d42bba2aa71217ba8c053947
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-08-23 15:04:00 +0530

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	  int a, b, sum, subtract, multiply, divide;
+  cout << "Enters two numbers : ";
+  cin >> a >> b;
+  
+  sum = a + b;
+  cout << "The sum is " << sum;
+  
+  subtract = a - b;
+  cout << "The subtraction is " << subtract;
+  
+  multiply = a * b;
+  cout << "The multiplication is " << multiply;
+  
+  divide = a / b;
+  cout << "The division is " << divide;
+ 
+}
