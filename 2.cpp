2170a8efee887c30e5cc48538db30f300f98522a
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-08-23 15:50:34 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
   int a, b, sum;
-  cout << "Enters two numbers to find their sum : ";
+  cout << "Enters two numbers to find their sum : "<<endl;
   cin >> a >> b;
   sum = a + b;
   cout << "The sum is " << sum;
